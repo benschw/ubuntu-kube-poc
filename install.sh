3fbf75ba36cfee4db77b0005d20703868d183365
Ben Schwartz <benschw@gmail.com>
2015-01-20 08:40:15 -0600

    fix(kubernetes): trying to get up to date binaries working, coreos still being a pain

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 
-mkdir -p /opt/kubernetes
 mkdir -p /opt/bin
-
 
 mkdir -p /vagrant/logs/nginx
 
 
+# wget -q https://dl.dropboxusercontent.com/u/1266213/kube-bin.tar.gz -O /opt/kube-bin.tar.gz
+# tar -xvf /opt/kube-bin.tar.gz -C /opt/
+# mv /opt/kube-bin/* /opt/bin/
 
-wget -q https://dl.dropboxusercontent.com/u/1266213/kubernetes-linux-amd64.tar.gz -O /opt/kubernetes-linux-amd64.tar.gz
-tar -xvf /opt/kubernetes-linux-amd64.tar.gz -C /opt/
-
-cp -r /opt/kubernetes-linux-amd64/* /opt/bin/
 
 # wget -q https://github.com/GoogleCloudPlatform/kubernetes/releases/download/v0.8.1/kubernetes.tar.gz -O /opt/kubernetes.tar.gz
 # tar -xvf /opt/kubernetes.tar.gz -C /opt
